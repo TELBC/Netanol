c625c174ab47e55427b102360192b82c92f6b5cc
Michal MALOVECKY <mal20422@spengergasse.at>
2023-07-19 18:25:17 +0200

    feat(docker): issue 4,6,8 together

---
+++ TAPAS/Tapas/Database/TapasDatabase.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Tapas.Database;
+
+public class TapasDatabase
+{
+    private readonly TapasContext _db;
+
+    public TapasDatabase(DbContextOptions<TapasContext> options)
+    {
+        _db = new TapasContext(options);
+        Console.WriteLine("Connetion to database established");
+    }
+    
+    public void Seed()
+    {
+        _db.Database.EnsureCreated();
+        _db.SaveChanges();
+    }
+}
