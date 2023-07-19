511475e13fdcc2f051dbeed44dfb99f15a2853f6
Denes TAKACS <tak20432@spengergasse.at>
2023-07-19 20:29:28 +0200

    feat(tapas): add DTOs and aggregated traces

--- TAPAS/Tapas/Controllers/TraceController.cs
+++ TAPAS/Tapas/Controllers/TraceController.cs
@@ -1,21 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Tapas.Database;
+using Tapas.Models;
 
-namespace Tapas.Controllers;
-
-[Route("trace")]
-public class TraceController : ControllerBase
+namespace Tapas.Controllers
 {
-    private readonly TraceRepository _traceRepository;
-
-    public TraceController(TraceRepository traceRepository)
+    [Route("trace")]
+    public class TraceController : ControllerBase
     {
-        _traceRepository = traceRepository;
-    }
+        private readonly TraceRepository _traceRepository;
+        private readonly IMapper _mapper;
 
-    [HttpGet("/get_all")]
-    public async Task<IActionResult> GetAllTraces()
-    {
-        return Ok(_traceRepository.GetAllSingleTraces());
+        public TraceController(TraceRepository traceRepository, IMapper mapper)
+        {
+            _traceRepository = traceRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet("/get_all")]
+        public async Task<IActionResult> GetAllTraces()
+        {
+            var traces = await _traceRepository.GetAllSingleTraces();
+            var traceDtos = _mapper.Map<IEnumerable<SingleTraceDto>>(traces); // this uses AutoMapper to map the traces to SingleTraceDto
+
+            var groupedTraces = traceDtos.GroupBy(dto => dto, new SingleTraceDtoEqualityComparer())
+                .Select(group => new
+                {
+                    Trace = group.Key,
+                    Count = group.Count()
+                });
+
+            return Ok(groupedTraces);
+        }
     }
 }
---
+++ TAPAS/Tapas/Database/Dto/SingleTraceDtoEqualityComparer.cs
@@ -0,0 +1,33 @@
+﻿namespace Tapas;
+
+public class SingleTraceDtoEqualityComparer : IEqualityComparer<SingleTraceDto>
+{
+    public bool Equals(SingleTraceDto x, SingleTraceDto y)
+    {
+        if (ReferenceEquals(x, y))
+            return true;
+
+        if (ReferenceEquals(x, null) || ReferenceEquals(y, null))
+            return false;
+
+        return x.Protocol == y.Protocol &&
+               x.SourceIpAddress == y.SourceIpAddress &&
+               x.SourcePort == y.SourcePort &&
+               x.DestinationIpAddress == y.DestinationIpAddress &&
+               x.DestinationPort == y.DestinationPort;
+    }
+    
+    public int GetHashCode(SingleTraceDto obj)
+    {
+        unchecked
+        {
+            int hash = 17;
+            hash = hash * 23 + obj.Protocol.GetHashCode();
+            hash = hash * 23 + (obj.SourceIpAddress?.GetHashCode() ?? 0);
+            hash = hash * 23 + obj.SourcePort.GetHashCode();
+            hash = hash * 23 + (obj.DestinationIpAddress?.GetHashCode() ?? 0);
+            hash = hash * 23 + obj.DestinationPort.GetHashCode();
+            return hash;
+        }
+    }
+}
