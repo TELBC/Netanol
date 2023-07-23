0b0419374f7e78373ff58087546f42d6e7a130dd
Michal MALOVECKY <mal20422@spengergasse.at>
2023-07-23 16:39:19 +0200

    fix(docker): Program.cs Postgres Configuration

--- TAPAS/Tapas/Program.cs
+++ TAPAS/Tapas/Program.cs
@@ -4,8 +4,13 @@ using Tapas.Listeners;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("PostgresConnection");
+var db = new TapasDatabase(new DbContextOptionsBuilder<TapasContext>().UseNpgsql(connectionString).Options);
+db.Seed();
+
 var collection = builder.Services;
-collection.AddDbContext<TapasContext>(optionsBuilder => optionsBuilder.UseInMemoryDatabase("Tapas"));
+// collection.AddDbContext<TapasContext>(optionsBuilder => optionsBuilder.UseInMemoryDatabase("Tapas"));
+collection.AddDbContext<TapasContext>(options => options.UseNpgsql(connectionString));
 collection.AddScoped<TraceRepository>();
 collection.AddHostedService<NetFlow9TraceImporter>(); // TODO: set exception behaviour
 
