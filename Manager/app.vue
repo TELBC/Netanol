b6ff58c4e6e086b827008f7673637b3d41164d31
LOSADA Tristan <Tristan.Losada@snt.at>
2023-08-05 18:52:28 +0200

    feat(frontend): basic network graph implementation + json parsing TAPAS-7

--- Manager/app.vue
+++ Manager/app.vue
@@ -1,8 +1,56 @@
+<script setup lang="ts">
+const jsonData = `{
+  "nodes": {
+    "1" :{
+      "id": 1,
+      "ipAddress": "192.168.1.12"
+    },
+    "2" :{
+      "id": 2,
+      "ipAddress": "10.5.12.254"
+    }
+  },
+  "traces": [
+    {
+      "sourceHostId": 1,
+      "destinationHostId": 2,
+      "count": 332
+    }
+  ]
+}`;
+
+const data = JSON.parse(jsonData);
+
+const nodes = {};
+const edges = {};
+
+for (const nodeId in data.nodes) {
+  const node = data.nodes[nodeId];
+  nodes[`node${nodeId}`] = { name: node.ipAddress, ipAddress: node.ipAddress };
+}
+
+data.traces.forEach((trace, index) => {
+  edges[`edge${index}`] = {
+    source: `node${trace.sourceHostId}`,
+    target: `node${trace.destinationHostId}`,
+    count: trace.count
+  };
+});
+
+</script>
+
 <template>
-  <div>
-    <NuxtLayout>
-      <NuxtPage />
-    </NuxtLayout>
-    <!-- <NuxtWelcome /> in case you want something displayed -->
-  </div>
+  <v-network-graph
+    class="graph"
+    :nodes="nodes"
+    :edges="edges"
+  />
 </template>
+
+<style>
+.graph {
+  width: 800px;
+  height: 600px;
+  border: 1px solid #000;
+}
+</style>
