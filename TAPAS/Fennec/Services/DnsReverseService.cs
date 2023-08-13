a16632d4d350b1281c32ec8772639cbb2b11df35
Michal MALOVECKY <mal20422@spengergasse.at>
2023-08-13 18:19:52 +0200

    perf(fennec): DnsReverseService add where clause for time frame
    added a where statement that only fetches NetworkHosts that weren't updated within a specified time period
    
    TAPAS-44

--- TAPAS/Fennec/Services/DnsReverseService.cs
+++ TAPAS/Fennec/Services/DnsReverseService.cs
@@ -5,56 +5,67 @@ using Fennec.Database.Domain.Technical;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fennec.Services;
-    
+
 public class DnsReverseService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly ILogger<DnsReverseService> _logger;
+
+    public DnsReverseService(IServiceProvider serviceProvider, ILogger<DnsReverseService> logger)
     {
-        private readonly IServiceProvider _serviceProvider;
-        private readonly ILogger<DnsReverseService> _logger;
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
 
-        public DnsReverseService(IServiceProvider serviceProvider, ILogger<DnsReverseService> logger)
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
         {
-            _serviceProvider = serviceProvider;
-            _logger = logger;
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            while (!stoppingToken.IsCancellationRequested)
+            using (var scope = _serviceProvider.CreateScope())
             {
-                using (var scope = _serviceProvider.CreateScope())
+                var dbContext = scope.ServiceProvider.GetRequiredService<TapasContext>();
+
+                // TODO: make configurable?
+                var cutOffTime = DateTimeOffset.UtcNow.AddDays(-1); // specifies which NetworkHosts to update (timeframe)
+
+                var hosts = await dbContext.NetworkHosts
+                    .Include(h => h.DnsInformation).Where(h =>
+                        h.DnsInformation == null || h.DnsInformation.LastAccessedDnsName <= cutOffTime)
+                    .ToListAsync(stoppingToken);
+
+                foreach (var host in hosts)
                 {
-                    var dbContext = scope.ServiceProvider.GetRequiredService<TapasContext>();
-
-                    var hosts = await dbContext.NetworkHosts
-                        .Include(h => h.DnsInformation)
-                        .ToListAsync(stoppingToken);
-
-                    foreach (var host in hosts)
+                    string resolvedDnsName;
+                    try
                     {
-                        string resolvedDnsName;
-                        try
-                        {
-                            resolvedDnsName = (await Dns.GetHostEntryAsync(host.IpAddress.ToString(), stoppingToken)).HostName;
-                        }
-                        catch (SocketException)
-                        {
-                            _logger.LogDebug("No DNS host found for IP address {IpAddress}. Defaulting to 'Unknown Hostname'", host.IpAddress);
-                            resolvedDnsName = "Unknown Hostname";
-                        }
-                        catch (Exception)
-                        {
-                            _logger.LogDebug("Unexpected error resolving IP address {IpAddress}. Defaulting to 'Unknown Hostname'", host.IpAddress);
-                            resolvedDnsName = "Unknown Hostname";
-                        }
-
-                        if (resolvedDnsName == host.DnsInformation?.DnsName) continue;
-                        host.DnsInformation!.DnsName = resolvedDnsName;
-                        host.DnsInformation.LastAccessedDnsName = DateTimeOffset.UtcNow;
+                        resolvedDnsName = (await Dns.GetHostEntryAsync(host.IpAddress.ToString(), stoppingToken))
+                            .HostName;
                     }
-                    await dbContext.SaveChangesAsync(stoppingToken);
+                    catch (SocketException)
+                    {
+                        _logger.LogDebug(
+                            "No DNS host found for IP address {IpAddress}. Defaulting to 'Unknown Hostname'",
+                            host.IpAddress);
+                        resolvedDnsName = "Unknown Hostname";
+                    }
+                    catch (Exception)
+                    {
+                        _logger.LogDebug(
+                            "Unexpected error resolving IP address {IpAddress}. Defaulting to 'Unknown Hostname'",
+                            host.IpAddress);
+                        resolvedDnsName = "Unknown Hostname";
+                    }
+
+                    if (resolvedDnsName == host.DnsInformation?.DnsName) continue;
+                    host.DnsInformation!.DnsName = resolvedDnsName;
+                    host.DnsInformation.LastAccessedDnsName = DateTimeOffset.UtcNow;
                 }
-                // still have to agree on an interval -> maybe make it configurable?
-                await Task.Delay(TimeSpan.FromSeconds(120), stoppingToken);
+
+                await dbContext.SaveChangesAsync(stoppingToken);
             }
+
+            // still have to agree on an interval -> maybe make it configurable?
+            await Task.Delay(TimeSpan.FromSeconds(120), stoppingToken);
         }
     }
+}
