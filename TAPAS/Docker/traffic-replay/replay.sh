ac2ec27d2e39ad039ce9afdf509593e5ebb132ae
Alexander ZIVNY-HARTIG <ziv20434@spengergasse.at>
2023-08-19 20:17:51 +0200

    test: add testing project
    Optimize traffic-replay files a bit to remove unnecessary
    waiting times.

--- TAPAS/Docker/traffic-replay/replay.sh
+++ TAPAS/Docker/traffic-replay/replay.sh
@@ -3,13 +3,9 @@
 echo "Extracting payloads... "
 
 # Extract UDP payloads from the modified pcap file
-tshark -r /captures/"$TRAFFIC_REPLAY_FILE" -T fields -e udp.payload -Y udp > /tmp/payloads.txt
+tshark -r /captures/"$TRAFFIC_REPLAY_FILE" -T fields -e udp.payload > /tmp/payloads.txt
 
-echo "Extracted payloads"
-
-sleep 3
-
-echo "Starting python script"
+echo "Extracted payloads & Starting python script"
 
 # Execute the Python script to send payloads
 python3 /send_payloads.py
