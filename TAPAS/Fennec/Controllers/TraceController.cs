e8dbaca1c186fb5c0b890cd9e5593f05a1fdd319
Alexander ZIVNY-HARTIG <ziv20434@spengergasse.at>
2023-08-20 02:04:24 +0200

    test: add tests for TraceController

--- TAPAS/Fennec/Controllers/TraceController.cs
+++ TAPAS/Fennec/Controllers/TraceController.cs
@@ -1,60 +1,40 @@
-﻿using System.Net;
-using AutoMapper;
-using Fennec.Database;
+﻿using Fennec.Database;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Fennec.Controllers;
 
-public class NetworkHostDto
-{
-    public long Id { get; set; }
-    public string IpAddress { get; set; }
-}
+public record NodeDto(long Id, string IpAddress);
 
+public record EdgeDto(long SourceHostId, long DestinationHostId, int Count);
+
+public record GraphDto(IDictionary<long, NodeDto> Nodes, List<EdgeDto> Edges);
 
 [Route("traces")]
 public class TraceController : ControllerBase
 {
-    private readonly TraceRepository _traceRepository;
     private readonly TapasContext _context;
-    private readonly IMapper _mapper;
 
-    public TraceController(TraceRepository traceRepository, IMapper mapper, TapasContext context)
+    public TraceController(TapasContext context)
     {
-        _traceRepository = traceRepository;
-        _mapper = mapper;
         _context = context;
     }
-    
+
     [HttpGet("get_by_window")]
     public async Task<IActionResult> GetTracesByWindow([FromQuery] DateTimeOffset from, [FromQuery] DateTimeOffset to)
     {
-        var traces = await _context.SingleTraces
+        var edges = await _context.SingleTraces
             .Where(trace => trace.Timestamp >= from && trace.Timestamp <= to)
             .GroupBy(trace => new { trace.SourceHostId, trace.DestinationHostId })
-            .Select(trace => new
-            {
-                trace.Key.SourceHostId,
-                trace.Key.DestinationHostId,
-                Count = trace.Count()
-            })
+            .Select(trace => new EdgeDto(trace.Key.SourceHostId, trace.Key.DestinationHostId, trace.Count()))
             .ToListAsync();
 
-        var hostIds = traces.Select(t => t.SourceHostId).Concat(traces.Select(t => t.DestinationHostId)).Distinct();
+        var hostIds = edges.Select(t => t.SourceHostId).Concat(edges.Select(t => t.DestinationHostId)).Distinct();
         var nodes = await _context.NetworkHosts
             .Where(host => hostIds.Contains(host.Id))
-            .Select(host => new NetworkHostDto
-            {
-                Id = host.Id,
-                IpAddress = host.IpAddress.ToString()
-            })
+            .Select(host => new NodeDto(host.Id, host.IpAddress.ToString()))
             .ToDictionaryAsync(host => host.Id);
-        
-        return Ok(new
-        {
-            Nodes = nodes,
-            Traces = traces
-        });
+
+        return Ok(new GraphDto(nodes, edges));
     }
 }
