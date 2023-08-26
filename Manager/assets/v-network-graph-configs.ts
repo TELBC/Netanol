9e8a2387aeed8b43c84e164d23ca2a557bb7dd14
Tristan Losada Benini <77665584+TELBC@users.noreply.github.com>
2023-08-26 18:26:06 +0200

    feat(frontend): automatic layouting of nodes in network graph
    * feat(frontend): automatic layouting of nodes in network graph
    
    added d3-force to the configs
    changed the default ForceLayout to be more adequate with our use case
    
    TAPAS-71
    
    * fix formatting, colors and font of network graph
    
    ---------
    
    Co-authored-by: Tristan LOSADA BENINI <los20421@spengergasse.at>

--- Manager/assets/v-network-graph-configs.ts
+++ Manager/assets/v-network-graph-configs.ts
@@ -1,6 +1,26 @@
 import { defineConfigs } from "v-network-graph"
+import {ForceEdgeDatum, ForceLayout, ForceNodeDatum} from "v-network-graph/lib/force-layout";
 
 export const networkGraphConfigs = defineConfigs({
+  node: {
+    normal: {
+      color: "#537B87",
+    },
+    hover: {
+      color: "#3E6474",
+    },
+    label: {
+      fontFamily: "'Open Sans', sans-serif",
+    }
+  },
+  edge: {
+    normal: {
+      color: "#7EA0A9",
+    },
+    hover: {
+      color: "#7095AB",
+    }
+  },
   view: {
     minZoomLevel: 1,
     maxZoomLevel: 8,
@@ -19,6 +39,16 @@ export const networkGraphConfigs = defineConfigs({
         width: 1,
         dasharray: 0,
       },
-    }
+    },
+    layoutHandler: new ForceLayout({
+        createSimulation: (d3, nodes, edges) => {
+          const forceLink = d3.forceLink<ForceNodeDatum, ForceEdgeDatum>(edges).id(d => d.id)
+          return d3.forceSimulation(nodes)
+            .force("edge", forceLink.distance(100).strength(2))
+            .force("charge", d3.forceManyBody().strength(-500))
+            .alphaMin(0.1)
+        }
+      }
+    )
   },
 })
