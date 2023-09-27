d9634e51dfff21af72025529338f272815f6d8bc
Alexander ZIVNY-HARTIG <ziv20434@spengergasse.at>
2023-09-27 19:35:42 +0200

    fix(fennec): migrate to removed NetworkDevice
    Also make ASPNET Core environment configurable

--- TAPAS/Fennec/Migrations/20230927172535_RemoveNetworkDevice.Designer.cs
+++ TAPAS/Fennec/Migrations/20230927172535_RemoveNetworkDevice.Designer.cs
@@ -4,6 +4,7 @@ using System.Net;
 using Fennec.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Fennec.Migrations
 {
     [DbContext(typeof(TapasContext))]
-    partial class TapasContextModelSnapshot : ModelSnapshot
+    [Migration("20230927172535_RemoveNetworkDevice")]
+    partial class RemoveNetworkDevice
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
