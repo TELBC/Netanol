5945bb86489e4a9d6a43f0598c7ed42401d2b036
ZIVNY-HARTIG Alexander <Alexander.Zivny-Hart@snt.at>
2023-10-10 17:24:48 +0200

    perf(replay): migrate to python
    Also add new IPFIX example traffic
    Remove the "v" from "netflowv9"
    Rework send_payloads.py to also convert data
    Use slimer python base image
    
    NETANOL-119

--- Docker/replay/send_payloads.py
+++ Docker/replay/send_payloads.py
@@ -1,10 +1,21 @@
-﻿print("Started python script", flush=True)
-
+﻿import subprocess
 import socket
 import time
 import os
 
-time.sleep(3)
+# Equivalent of the bash script
+print("Extracting payloads...", flush=True)
+
+time.sleep(2)
+
+try:
+    REPLAY_FILE = os.environ['REPLAY_FILE']
+    subprocess.run(["tshark", "-r", f"/captures/{REPLAY_FILE}", "-T", "fields", "-e", "udp.payload"], stdout=open('/tmp/payloads.txt', 'w'))
+except Exception as e:
+    print(f"Failed to extract payloads: {e}", flush=True)
+    exit(1)
+
+print("Extracted payloads & Starting python script", flush=True)
 
 # Configuration
 target = os.environ['REPLAY_TARGET'].split(':')
@@ -14,27 +25,41 @@ UDP_PORT = int(target[1])
 print(f"Target: {UDP_IP}:{UDP_PORT}", flush=True)
 
 print("Reading payloads", flush=True)
+
 # Convert hex payloads to binary
-with open('/tmp/payloads.txt', 'r') as f:
-    payloads = [bytes.fromhex(line.strip()) for line in f]
-print("Read payloads", flush=True)
+try:
+    with open('/tmp/payloads.txt', 'r') as f:
+        payloads = [bytes.fromhex(line.strip()) for line in f]
+except Exception as e:
+    print(f"Failed to read payloads: {e}", flush=True)
+    exit(1)
+
+total_packets = len(payloads)
+print(f"Read {total_packets} payloads", flush=True)
 
 # Send payloads
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.settimeout(5)
 idx = 0
+sent_packets = 0
+
 while True:
-    idx += 1
-    payload = payloads[idx % len(payloads)]
+    current_packet_idx = idx % total_packets
+    payload = payloads[current_packet_idx]
+    packet_size = len(payload)
+
     try:
         sock.sendto(payload, (UDP_IP, UDP_PORT))
+        sent_packets += 1
     except Exception as e:
         print(e, flush=True)
         print("Failed to send packet -> Retrying in 10 seconds", flush=True)
         time.sleep(10)
         continue
-        
-    print(f"Sent packet num: {idx}", flush=True)
-    time.sleep(1)
     
+    print(f"Packet {current_packet_idx + 1}/{total_packets} with {packet_size} bytes, Total Packets: {sent_packets}", flush=True)
+    
+    idx += 1
+    time.sleep(1)
+
 sock.close()
