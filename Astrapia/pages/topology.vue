c7f9d8e40c5b6bab83689a62f8333cf26f844394
Tristan Losada Benini <77665584+TELBC@users.noreply.github.com>
2023-10-26 19:49:11 +0200

    feat(frontend): user interaction with nodes
    * feat(frontend): add tooltip
    
    hoverable node shows tooltip with node name(ip)
    
    NETANOL-70
    
    * fix(frontend): fix duplicate nodes
    
    fix the duplicate node bug
    should be faster
    
    NETANOL-70
    
    * fix(frontend): fix graph jitter
    
    rerender only triggered when new data introduced
    removed log to check layout existence
    
    NETANOL-70
    
    * fix(frontend): fix unnecessary variables
    
    remove oldNodes/oldEdges
    
    NETANOL-70
    
    * fix(frontend): remove blank line
    
    NETANOL-70
    
    ---------
    
    Co-authored-by: Tristan LOSADA BENINI <los20421@spengergasse.at>

--- Astrapia/pages/topology.vue
+++ Astrapia/pages/topology.vue
@@ -4,46 +4,71 @@
     :nodes="nodes"
     :edges="edges"
     :configs="networkGraphConfigs"
+    :event-handlers="eventHandlers"
   >
     <template #edge-label="{ edge, hovered, ...slotProps }">
       <v-edge-label v-if="hovered" :text="edge.label" align="center" vertical-align="above" v-bind="slotProps" />
     </template>
   </v-network-graph>
+    <div id="tooltip"/>
   </div>
 </template>
 
 <script setup lang="ts">
-import {Edges, Nodes, VEdgeLabel, VNetworkGraph} from 'v-network-graph'
-import { networkGraphConfigs } from 'assets/v-network-graph-configs'
-import { parseJsonData } from "~/utils/networkGraphParsing";
-import { createLayout, fetchGraphData } from "~/services/graphService";
+import { ref, onMounted, onBeforeUnmount } from 'vue';
+import { Edges, Nodes, VEdgeLabel, VNetworkGraph } from 'v-network-graph';
+import { networkGraphConfigs } from 'assets/v-network-graph-configs';
+import { parseJsonData } from '~/utils/networkGraphParsing';
+import { createLayout, fetchGraphData } from '~/services/graphService';
+import * as vNG from 'v-network-graph';
+import { isEqual } from 'smob';
 
-const nodes = ref<Nodes[]>([])
-const edges = ref<Edges[]>([])
-const layout = "test";
-const dateRange = { from: "2022-01-01", to: "2025-01-01"}//TODO date range UTC.now-2min
+const nodes = ref<Nodes[]>([]);
+const edges = ref<Edges[]>([]);
+const layout = 'test';
+const dateRange: { from: string; to: string } = { from: '2022-01-01', to: '2025-01-01' }; // TODO date range UTC.now-2min
 
-const fetchGraphDataPeriodically = async () => {
-  try {
-    const data = await fetchGraphData(dateRange, layout)
-    const { nodes: parsedNodes, edges: parsedEdges } = parseJsonData(data.nodes, data.edges)
-    nodes.value = parsedNodes
-    edges.value = parsedEdges
-  } catch (error) {
-    console.error(error)
+const eventHandlers: vNG.EventHandlers = {
+  'node:pointerover': ({ node, event }) => {
+    const tooltip = document.getElementById('tooltip');
+    tooltip.style.visibility = 'visible';
+    tooltip.textContent = `${nodes.value[node].name}`;
+    tooltip.style.left = `${event.clientX}px`;
+    tooltip.style.top = `${event.clientY}px`;
+  },
+  'node:pointerout': () => {
+    const tooltip = document.getElementById('tooltip');
+    tooltip.style.visibility = 'hidden';
+  },
+};
+
+const updateGraphData = (parsedNodes: Nodes[], parsedEdges: Edges[]) => {
+  if (!isEqual(nodes.value, parsedNodes) || !isEqual(edges.value, parsedEdges)) {
+    nodes.value = parsedNodes;
+    edges.value = parsedEdges;
   }
-}
+};
+
+const fetchGraphDataPeriodically: () => Promise<void> = async () => {
+  try {
+    const data = await fetchGraphData(dateRange, layout);
+    const { nodes: parsedNodes, edges: parsedEdges } = parseJsonData(data.nodes, data.edges);
+    updateGraphData(parsedNodes, parsedEdges);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 onMounted(async () => {
-  await createLayout(layout)
-  await fetchGraphDataPeriodically()
-
-  const fetchInterval = setInterval(fetchGraphDataPeriodically, 5000)
+  const fetchInterval = setInterval(fetchGraphDataPeriodically, 5000);
 
   onBeforeUnmount(() => {
-    clearInterval(fetchInterval)
-  })
-})
+    clearInterval(fetchInterval);
+  });
+
+  await createLayout(layout);
+  await fetchGraphDataPeriodically();
+});
 </script>
 
 <style scoped>
@@ -52,4 +77,16 @@ onMounted(async () => {
   width: 100vw;
   height: 100vh;
 }
+#tooltip {
+  position: absolute;
+  visibility: hidden;
+  background-color: #D7DFE7;
+  color: black;
+  padding: 2px;
+  border-radius: 5px;
+  text-align: center;
+  font-family: 'Open Sans', sans-serif;
+  font-size: 0.8rem;
+}
+
 </style>
