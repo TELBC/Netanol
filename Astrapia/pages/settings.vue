b83d53a3623ddfc110892d04d7c49af8ada90472
necoder27 <95191364+necoder27@users.noreply.github.com>
2023-10-31 14:18:32 +0100

    feat(frontend): storing JWT token in Session Storage instead of nuxt-auth, sidebar only visible when logged in

--- Astrapia/pages/settings.vue
+++ Astrapia/pages/settings.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    Settings
+    <tapas-alert-dialogue v-if="showAlert" />
+    <button @click="showAlert = true">Alert</button>
   </div>
 </template>
 
 <script setup lang="ts">
-
+definePageMeta({
+  middleware: ["auth"]
+})
+let showAlert = false;
 </script>
 
 <style scoped>
-
+button {
+  margin-left: 50vw;
+}
 </style>
