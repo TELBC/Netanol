3af58c06fe4aeb14b62e3e2c5eb1c9d895f1ed39
Denes TAKACS <denes.takacs@protonmail.com>
2023-11-12 20:23:02 +0100

    feat(fennec): add MetricService

---
+++ Packrat/Fennec/Controllers/MetricController.cs
@@ -0,0 +1,29 @@
+﻿using Fennec.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fennec.Controllers;
+
+[ApiController]
+//[Authorize]
+[Route("metrics")]
+public class MetricController : ControllerBase
+{
+    private readonly MetricService _metricService;
+
+    public MetricController(MetricService metricService)
+    {
+        _metricService = metricService;
+    }
+   
+    [HttpGet("all")]
+    public async Task<IActionResult> GetAllMetric()
+    {
+        var (count, byteCountSum) = await _metricService.GetAllMetrics();
+        return Ok(new 
+        {
+            count = count,
+            byteCountSum = byteCountSum.GetValue("totalByteCount").AsInt64
+        });
+    }
+}
---
+++ Packrat/Fennec/Services/MetricService.cs
@@ -0,0 +1,28 @@
+﻿using Fennec.Database.Domain;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Fennec.Services;
+
+public class MetricService
+{
+    private readonly IMongoCollection<SingleTrace> _singleTraces;
+    private readonly IMongoClient _mongoClient;
+
+    public MetricService(IMongoCollection<SingleTrace> singleTraces, IMongoClient mongoClient)
+    {
+        _singleTraces = singleTraces;
+        _mongoClient = mongoClient;
+    }
+    
+    public async Task<(long, BsonDocument)> GetAllMetrics()
+    {
+        var count = await _singleTraces.CountDocumentsAsync(trace => true);
+        var byteCountSum = await _singleTraces.Aggregate()
+            .Group(new BsonDocument { { "_id", 1 }, { "totalByteCount", new BsonDocument { { "$sum", "$byteCount" } } } })
+            .FirstOrDefaultAsync();
+
+        return (count, byteCountSum);
+    }
+
+}
