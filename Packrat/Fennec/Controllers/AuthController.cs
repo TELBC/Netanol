fcb7a5e01c6f94f36205fa790c554840fb608b70
Alexander ZIVNY-HARTIG <alexander.zivny-hartig@protonmail.com>
2023-11-16 23:04:31 +0100

    feat(fennec): replace jwt with bff cookies
    NETANOL-163

--- Packrat/Fennec/Controllers/AuthController.cs
+++ Packrat/Fennec/Controllers/AuthController.cs
@@ -1,7 +1,12 @@
-﻿using Fennec.Database;
-using Fennec.Services;
+﻿using System.Diagnostics;
+using System.Security.Claims;
+using Fennec.Database;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Fennec.Controllers;
 
@@ -11,20 +16,31 @@ public record LoginRequest(string Username, string Password);
 [ApiController]
 public class AuthController : ControllerBase
 {
-    private readonly IJwtService _jwtService;
+    private readonly ILogger _log;
     private readonly SignInManager<NetanolUser> _signInManager;
     private readonly UserManager<NetanolUser> _userManager;
-    private readonly ILogger _log;
 
-    public AuthController(SignInManager<NetanolUser> signInManager, ILogger log, IJwtService jwtService, UserManager<NetanolUser> userManager)
+    public AuthController(SignInManager<NetanolUser> signInManager, ILogger log, UserManager<NetanolUser> userManager)
     {
         _signInManager = signInManager;
-        _jwtService = jwtService;
         _userManager = userManager;
         _log = log.ForContext<AuthController>();
     }
 
+    /// <summary>
+    ///     Sign in using the passed credentials.
+    /// </summary>
+    /// <remarks>
+    ///     Try to log in using the username and password in the <see cref="LoginRequest" />. Once completed the backend
+    ///     will tell the browser, via HTTP headers, to set a cookie which will be transmitted on all subsequent
+    ///     requests. This means the frontend does not have to handle tokens.
+    /// </remarks>
+    /// <param name="request"></param>
+    /// <returns></returns>
     [HttpPost("login")]
+    [SwaggerResponse(StatusCodes.Status204NoContent, "The application is now logged in.")]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized,
+        "The login attempt failed, reference the message for more information.")]
     public async Task<IActionResult> Login([FromBody] LoginRequest request)
     {
         if (!ModelState.IsValid)
@@ -32,13 +48,13 @@ public class AuthController : ControllerBase
 
         _log.Debug("Attempting login for {Username}", request.Username);
         var user = await _userManager.FindByNameAsync(request.Username);
-        
+
         if (user == null)
         {
             _log.Debug("Login attempt for {Username} failed... User doesn't exist", request.Username);
             return Unauthorized("Invalid credentials");
         }
-        
+
         var result = await _signInManager.PasswordSignInAsync(user, request.Password, false, true);
         if (result.IsNotAllowed)
         {
@@ -60,7 +76,60 @@ public class AuthController : ControllerBase
 
         _log.Debug("Login attempt for {Username} succeeded -> Generating JWT token", request.Username);
         var roles = await _userManager.GetRolesAsync(user);
-        var token = _jwtService.GenerateJwtToken(user, roles);
-        return Ok(new { token });
+
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, request.Username),
+            new(ClaimTypes.Role, "admin")
+        };
+
+        var claimsIdentity = new ClaimsIdentity(
+            claims,
+            CookieAuthenticationDefaults.AuthenticationScheme);
+
+        var authProperties = new AuthenticationProperties
+        {
+            AllowRefresh = true,
+            ExpiresUtc = DateTimeOffset.UtcNow.AddHours(3)
+        };
+
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity),
+            authProperties);
+
+        return NoContent();
+    }
+
+    /// <summary>
+    ///     Check if the application is still authenticated.
+    /// </summary>
+    /// <remarks>
+    ///     This is intended to be used to check if the frontend is still logged in via the cookie.
+    /// </remarks>
+    /// <returns></returns>
+    [Authorize]
+    [SwaggerResponse(StatusCodes.Status200OK, "The request is authenticated.")]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized, "The request is not authenticated.")]
+    [HttpGet("status")]
+    public IActionResult Status()
+    {
+        return NoContent();
+    }
+
+    /// <summary>
+    ///     Logout and remove the authentication cookie from storage.
+    /// </summary>
+    /// <remarks>
+    ///     TODO: currently broken
+    /// </remarks>
+    /// <returns></returns>
+    [HttpGet("logout")]
+    [SwaggerResponse(StatusCodes.Status204NoContent,
+        "The request completed and the application is no longer authenticated.")]
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync();
+        return NoContent();
     }
 }
--- Packrat/Fennec/Options/SecurityOptions.cs
+++ Packrat/Fennec/Options/SecurityOptions.cs
@@ -8,20 +8,9 @@ public class SecurityOptions
     [Required] 
     public AccessOptions Access { get; set; }
 
-    [Required] 
-    public JwtOptions Jwt { get; set; }
-
     public class AccessOptions
     {
         public string Username { get; set; }
         public string Password { get; set; }
-        public bool HideWhenNoAuth { get; set; }
-    }
-
-    public class JwtOptions
-    {
-        public string Issuer { get; set; } = "netanol";
-        public string Audience { get; set; } = "netanol";
-        public string? Key { get; set; }
     }
 }
