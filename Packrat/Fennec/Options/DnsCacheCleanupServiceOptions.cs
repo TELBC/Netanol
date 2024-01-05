61436756abfd200276541ee2e22fb7bba127bf4e
Michal MALOVECKY <mal20422@spengergasse.at>
2024-01-05 18:05:46 +0100

    feat(fennec): add DnsCheckService
    DnsCheckService is set to an interval and checks/updates with new DNS entries. DnsResolverService has a _staleEntryDuration which specifies after what time a DNS entry is removed from the cache.

---
+++ Packrat/Fennec/Options/DnsCacheCleanupServiceOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Fennec.Options;
+
+public class DnsCacheCleanupServiceOptions
+{
+    /// <summary>
+    /// The interval at which the DNS cache is cleaned up.
+    /// </summary>
+    public TimeSpan CleanupInterval { get; set; } = TimeSpan.Parse("00:06:00:00"); // format: dd:hh:mm:ss
+}
