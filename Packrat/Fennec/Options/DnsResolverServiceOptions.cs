a22e9406a9e63a244743ae3c7981fbff9ad2829c
Michal MALOVECKY <mal20422@spengergasse.at>
2024-01-19 10:30:21 +0100

    fix(fennec): remove DnsCheckService and refac DnsResolverService
    Removed DnsCheckService and changed the DnsOptions into one (DnsCacheOptions)

--- Packrat/Fennec/Options/DnsResolverServiceOptions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fennec.Options;
-
-public class DnsResolverServiceOptions
-{
-    /// <summary>
-    /// The duration after which a DNS entry is considered stale and removed from the cache.
-    /// </summary>
-    public TimeSpan StaleEntryDuration { get; set; } = TimeSpan.Parse("02:00:00:00"); // format: dd:hh:mm:ss
-}
