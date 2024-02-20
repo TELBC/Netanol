fc42476decb2b5fa80c864ffc3e62f1266c75151
Laetitia EL BADRY <95191364+necoder27@users.noreply.github.com>
2024-02-20 20:10:36 +0100

    feature(astrapia): finish layer service

--- Astrapia/services/layerService.ts
+++ Astrapia/services/layerService.ts
@@ -22,9 +22,25 @@ export interface Layer {
 }
 
 class LayerService {
+  public async getLayer(layoutName: string, index: number) {
+    return await ApiService.get<Layer>(`/api/layout/${layoutName}/layers/${index}`).then(x => x.data);
+  }
+
   public async createLayer(layer: Layer, layoutName: string) {
     return ApiService.post(`/api/layout/${layoutName}/layers`, layer);
   }
 
+  public async moveLayer(layoutName: string, oldIndex: number, newIndex: number) {
+    return ApiService.put(`/api/layout/${layoutName}/layers/${oldIndex}/${newIndex}`);
+  }
 
+  public async editLayer(layoutName: string, index: number) {
+    return ApiService.put(`/api/layout/${layoutName}/layers/${index}`);
+  }
+
+public async deleteLayer(layoutName: string, index: number) {
+    return ApiService.delete(`/api/layout/${layoutName}/layers/${index}`);
+  }
 }
+
+export default new LayerService();
