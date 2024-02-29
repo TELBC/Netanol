1e2f62c600441a22183d1cb5f187ea2f00711275
Michal MALOVECKY <mal20422@spengergasse.at>
2024-02-29 14:29:39 +0100

    test(parser): add ipfix,netflow v5 and v9 test
    Added implementation tests for IPFIX, NetFlow9 and NetFlow5.

---
+++ Packrat/Fennec.Tests/Parsers/NetFlow9ParserTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Fennec.Parsers;
+using Fennec.Services;
+using NSubstitute;
+using Serilog;
+
+namespace Fennec.Tests.Parsers;
+
+public class NetFlow9ParserTests
+{
+    [Fact]
+    public void ParseNetFlow9Message()
+    {
+        // Arrange
+        var substituteLogger = Substitute.For<ILogger>();
+        var substituteMetricService = Substitute.For<IMetricService>();
+
+        var parser = new NetFlow9Parser(substituteLogger, substituteMetricService);
+        var netflowTemplates = Convert.FromBase64String(
+            "AAkAAgBaBI9k0SU7AAAAaAAAAAIBAwBRAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAzC8AxwKgAlgG71i0GAAAAAQBZiYAAWYmAAAApaQAAAAAAAABAAAAAAAAAAAEAAAA8AQMADQAbABAAHAAQAAgABAAMAAQABwACAAsAAgAEAAEAMAAEABYABAAVAAQACgAEAAEACAACAAg=");
+        var netflowData = Convert.FromBase64String(
+            "AAkAAwBZtm9k0SUnAAAAZwAAAAIBAwDrAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACin4bqwKgAlgG70kIGAAAAAQBY7TMAWTtgAAApaQAAAAAAAAaoAAAAAAAAAAMAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAJJCmzbAqACWaYz32AYAAAABAFk7YABZO2AAAClpAAAAAAAAASYAAAAAAAAAAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEkIae8CoAJYBu9ZgBgAAAAEAWTtgAFk7YAAAKWkAAAAAAAAAjAAAAAAAAAAC");
+
+        var templatesUdpReceiveResult =
+            new UdpReceiveResult(netflowTemplates, new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0));
+        var dataUdpReceiveResult = new UdpReceiveResult(netflowData, new IPEndPoint(IPAddress.Parse("127.0.0.1"), 0));
+
+        // Act
+        var templates = parser.Parse(templatesUdpReceiveResult);
+        var data = parser.Parse(dataUdpReceiveResult);
+
+        // Assert
+        Assert.NotNull(data);
+        Assert.Single(data);
+    }
+}
